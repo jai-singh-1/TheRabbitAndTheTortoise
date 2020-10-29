0636c2ae9a8171c49ad5ec35022965553780c003
jai_singh_1 <jai.singh_cs19@gla.ac.in>
2020-10-29 11:31:14 +0530

    override the run() method and for loop and switch case in Tortoise.java

--- src/thread/tortoise/Tortoise.java
+++ src/thread/tortoise/Tortoise.java
@@ -5,6 +5,7 @@ import javax.swing.*;
 public class Tortoise extends Thread {
     public static final int MILESTONES = 5;
     private final Thread tortoise;
+
     public Tortoise() {
         this.tortoise = new Thread(this, "Torotise");
     }
@@ -12,5 +13,29 @@ public class Tortoise extends Thread {
     public Thread getTortoise() {
         return tortoise;
     }
+
+    @Override
+    public void run() {
+        for (int index = 0; index < MILESTONES; index++) {
+            switch (index) {
+                case 0:
+                    System.out.println("The Tortoise has started from the start line!");
+                    break;
+                case 1:
+                    System.out.println("The Tortoise has reached the Cool River!");
+                    break;
+                case 2:
+                    System.out.println("The Tortoise has reached the Mountain Hill!");
+                    break;
+                case 3:
+                    System.out.println("The Tortoise has reached the Big Oak Tree!");
+                    break;
+                case 4:
+                    System.out.println("The Tortoise has reached the finish line!");
+                    break;
+            }
+    }
 }
+
+
 
