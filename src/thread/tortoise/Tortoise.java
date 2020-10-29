947588b480f19af7f6ba7b1b3eb6665b48359e40
jai_singh_1 <jai.singh_cs19@gla.ac.in>
2020-10-29 11:26:06 +0530

    add Constructor and getter method in Tortoise.java

--- src/thread/tortoise/Tortoise.java
+++ src/thread/tortoise/Tortoise.java
@@ -4,5 +4,13 @@ import javax.swing.*;
 
 public class Tortoise extends Thread {
     public static final int MILESTONES = 5;
+    private final Thread tortoise;
+    public Tortoise() {
+        this.tortoise = new Thread(this, "Torotise");
+    }
+
+    public Thread getTortoise() {
+        return tortoise;
+    }
 }
 
